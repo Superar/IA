9023b18dabe638eac9ca0149377bf707c18eec69
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-10-27 19:06:08 -0200

    Correções na interface

--- T1/interface.py
+++ T1/interface.py
@@ -9,18 +9,11 @@ obj_map = {'pacman': '<',
            'fantasma': 'x',
            'cereja': 'C'}
 
-dim = sys.stdin.readline()
-dim = dim.split('x')
-dim = [int(axis) for axis in dim]
 
-lines = sys.stdin.readlines()
-for num_exec, line in enumerate(reversed(lines)):
-    print('Execucao numero: ' + str(num_exec))
-    line = line.translate({ord(c): None for c in '[]\n'})
-    print(line.split(','))
+def print_game(dim, line):
     for i, elem in enumerate(line.split(',')):
         if i % dim[0] == 0:
-            print('-----------------------------')
+            print('-----' * dim[0])
             print('|', end='')
             print('   ' + obj_map[elem], end='   ')
         elif i % dim[0] == dim[0] - 1:
@@ -28,6 +21,33 @@ for num_exec, line in enumerate(reversed(lines)):
             print('|')
         else:
             print(obj_map[elem], end='   ')
-    print('-----------------------------')
+    print('-----' * dim[0])
+
+
+def print_game_over(dim):
+    print('\n' * (dim[1] // 2))
+    print((' ' * (dim[0] // 3)) + 'GAME OVER')
+    time.sleep(1.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Le as dimensoes do tabuleiro
+dim = sys.stdin.readline()
+dim = dim.split('x')
+dim = [int(axis) for axis in dim]
+
+# Le dados das execucoes
+lines = sys.stdin.readlines()
+
+num_exec = 1
+for line in reversed(lines):
+    print('Execucao numero: ' + str(num_exec))
+    line = line.translate({ord(c): None for c in '[]\n'})
+
+    print_game(dim, line)
     time.sleep(0.5)
     os.system('cls' if os.name == 'nt' else 'clear')
+
+    if 'cereja' not in line.split(','):
+        num_exec = num_exec + 1
+        print_game_over(dim)
