23be18b4fa99f43dd40a51b0e21012c5e69391cf
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-12-03 20:21:24 -0200

    Correção na geração dos labels do dataset emnist

--- T2/emnist_dataset.py
+++ T2/emnist_dataset.py
@@ -9,10 +9,12 @@ class EMNISTDataset(object):
         matlab_data = loadmat(file_path)
         self.train_data = matlab_data['dataset'][0][0][0][0][0][0]
         self.train_data_labels = matlab_data['dataset'][0][0][0][0][0][1].flatten()
+        self.train_data_labels = self.train_data_labels - np.min(self.train_data_labels)
         self.num_classes = len(unique(self.train_data_labels))
         self.data_dim = self.train_data.shape[1]
         self.test_data = matlab_data['dataset'][0][0][1][0][0][0]
         self.test_data_labels = matlab_data['dataset'][0][0][1][0][0][1].flatten()
+        self.test_data_labels = self.test_data_labels - np.min(self.test_data_labels)
 
 
     def get_normalized_data(self):
