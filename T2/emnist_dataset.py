e04a39472215051d15221697bb6264d000630fab
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-12-03 11:19:41 -0200

    Atualização para o uso do conjunto de dados EMNIST

---
+++ T2/emnist_dataset.py
@@ -0,0 +1,24 @@
+from scipy.io import loadmat
+from keras.utils import to_categorical
+from numpy.lib.arraysetops import unique
+import numpy as np
+
+class EMNISTDataset(object):
+
+    def __init__(self, file_path):
+        matlab_data = loadmat(file_path)
+        self.train_data = matlab_data['dataset'][0][0][0][0][0][0]
+        self.train_data_labels = matlab_data['dataset'][0][0][0][0][0][1].flatten()
+        self.num_classes = len(unique(self.train_data_labels))
+        self.data_dim = self.train_data.shape[1]
+        self.test_data = matlab_data['dataset'][0][0][1][0][0][0]
+        self.test_data_labels = matlab_data['dataset'][0][0][1][0][0][1].flatten()
+
+
+    def get_normalized_data(self):
+        max_val = np.max(self.train_data.flatten())
+        return self.train_data / max_val, self.test_data / max_val
+
+    def get_categorical_labels(self):
+        return (to_categorical(self.train_data_labels, num_classes=self.num_classes),
+                to_categorical(self.test_data_labels, num_classes=self.num_classes))
--- T2/nn_classifier.py
+++ T2/nn_classifier.py
@@ -1,40 +1,41 @@
 # coding=utf-8
 
 from usps_dataset import USPSDataset
+from emnist_dataset import EMNISTDataset
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 
 usps_dataset = USPSDataset('usps/usps.train', 'usps/usps.test')
+emnist_dataset = EMNISTDataset('emnist/emnist-balanced.mat')
 
 usps_train_data, usps_test_data = usps_dataset.get_normalized_data()
 usps_train_categorical_labels, usps_test_categorical_labels = usps_dataset.get_categorical_labels()
 
+emnist_train_data, emnist_test_data = emnist_dataset.get_normalized_data()
+emnist_train_categorical_labels, emnist_test_categorical_labels = emnist_dataset.get_categorical_labels()
+
 #Criação do modelo
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(usps_dataset.data_dim,)))
+model.add(Dense(512, activation='relu', input_shape=(emnist_dataset.data_dim,)))
 model.add(Dense(512, activation='relu', input_shape=(None, 512)))
-model.add(Dense(10, activation='softmax', input_shape=(None, 512)))
+model.add(Dense(emnist_dataset.num_classes, activation='softmax', input_shape=(None, 512)))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop',
-              metrics=['categorical_accuracy', 'categorical_crossentropy'])
+              metrics=['categorical_accuracy'])
 
 # Treinamento
-history = model.fit(usps_train_data, usps_train_categorical_labels,
+history = model.fit(emnist_train_data, emnist_train_categorical_labels,
                     epochs=20,
-                    validation_data=(usps_test_data, usps_test_categorical_labels))
+                    validation_data=(emnist_test_data, emnist_test_categorical_labels))
+model.save('modelo_emnist_balanced.h5')
 
 # Avaliação do modelo
-score_depois = model.evaluate(usps_test_data, usps_test_categorical_labels)
+score_depois = model.evaluate(emnist_test_data, emnist_test_categorical_labels)
 
 print("\nPrecisão: {}%".format(score_depois[1]*100))
 
 plt.figure(1)
-plt.subplot(121)
 plt.plot(history.history['categorical_accuracy'],
          marker='o', linestyle='solid', color='b')
-
-plt.subplot(122)
-plt.plot(history.history['categorical_crossentropy'],
-         marker='o', linestyle='solid', color='r')
 plt.show()
